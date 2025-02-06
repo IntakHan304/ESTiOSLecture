f00ba5e2c68214272a6ad22ee2d6776fd4aeafcb
InTak Han <hits0304@gmail.com>
2025-02-06 15:01:11 +0900

    모델 추가 및 수정2

--- SwiftDay06/SwiftDay06/ShapeView/ShapeView.swift
+++ SwiftDay06/SwiftDay06/ShapeView/ShapeView.swift
@@ -1,21 +1,5 @@
 
-//Super class
-class Shape {
-    func draw(){
-        print("도형을 그립니다")
-    }
-}
 
-class Circle: Shape {
-    override func draw() {
-        print("원을 그립니다.")
-    }
-}
-class Rectangle: Shape {
-    override func draw() {
-        print("사각형을 그립니다.")
-    }
-}
 
 class ShapeView {
     // Array 선언
